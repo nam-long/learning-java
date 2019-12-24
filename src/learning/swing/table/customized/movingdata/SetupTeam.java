04c44b4465d50a980495a9f8b3ef247af8dfc0ac
trannamlong <long.trannam@hotmail.com>
2019-12-24 09:48:43 +0700

    - update

--- src/learning/swing/table/customized/movingdata/SetupTeam.java
+++ src/learning/swing/table/customized/movingdata/SetupTeam.java
@@ -2,8 +2,7 @@ package learning.swing.table.customized.movingdata;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.List;
 
 public class SetupTeam extends JFrame {
@@ -24,35 +23,116 @@ public class SetupTeam extends JFrame {
         setContentPane(rootPanel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        String[] columnNames = {"ID", "Full Name", "Salary"};
+        initComponents();
+    }
 
-        modelAvailableEmployees = new EmployeesTableModel(columnNames);
-        modelAvailableEmployees.addEmployee(new Employee(0, "Messi", 500000));
-        modelAvailableEmployees.addEmployee(new Employee(2, "Ronaldo", 600000));
-        modelAvailableEmployees.addEmployee(new Employee(3, "Bale", 400000));
-        modelAvailableEmployees.addEmployee(new Employee(4, "Rooney", 300000));
-        tableAvailableEmployees.setModel(modelAvailableEmployees);
+    private void initComponents() {
 
-        modelTeamMembers = new EmployeesTableModel(columnNames);
-        tableTeamMembers.setModel(modelTeamMembers);
+        initTables();
 
         selectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /**
+                 * Chỉ số của những dòng đang được chọn trong bảng.
+                 *
+                 * Lưu ý: với cách cài đặt danh sách (List) cho dữ liệu của bảng,
+                 * thì thứ tự các phần tử trong danh sách cũng tương ứng với thứ tự
+                 * của các phần tử trong bảng.
+                 */
                 int[] rowIndexes = tableAvailableEmployees.getSelectedRows();
-                List<Employee> removedEmployees = modelAvailableEmployees.removeEmployee(rowIndexes);
-                modelTeamMembers.addEmployees(removedEmployees);
+                if (rowIndexes.length > 0) {
+                    onSelect(rowIndexes);
+                }
             }
         });
 
         deselectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /**
+                 * Chỉ số của những dòng đang được chọn trong bảng.
+                 *
+                 * Lưu ý: với cách cài đặt danh sách (List) cho dữ liệu của bảng,
+                 * thì thứ tự các phần tử trong danh sách cũng tương ứng với thứ tự
+                 * của các phần tử trong bảng.
+                 */
                 int[] rowIndexes = tableTeamMembers.getSelectedRows();
-                List<Employee> removedEmployees = modelTeamMembers.removeEmployee(rowIndexes);
-                modelAvailableEmployees.addEmployees(removedEmployees);
+                if (rowIndexes.length > 0) {
+                    onDeselect(rowIndexes);
+                }
             }
         });
+    }
+
+    private void initTables() {
+
+        String[] columnNames = {"ID", "Full Name", "Salary"};
+
+        modelAvailableEmployees = new EmployeesTableModel(columnNames);
+        tableAvailableEmployees.setModel(modelAvailableEmployees);
+        tableAvailableEmployees.addMouseListener(new MouseAdapter() {
+
+            /**
+             * Cài đặt sự kiện Double-Clicked: employee được chọn sẽ di chuyển sang
+             * bảng Team Members.
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                // Đếm số lần click
+                if (e.getClickCount() > 1) {
+                    // Xác định dòng đang được chọn từ tọa độ của một điểm (Point) trên bảng
+                    int rowIndex = tableAvailableEmployees.rowAtPoint(e.getPoint());
+                    if (rowIndex != -1) {
+                        int[] rowIndexes = {rowIndex};
+                        onSelect(rowIndexes);
+                    }
+                }
+            }
+        });
+
+        modelTeamMembers = new EmployeesTableModel(columnNames);
+        tableTeamMembers.setModel(modelTeamMembers);
+        tableTeamMembers.addMouseListener(new MouseAdapter() {
+
+            /**
+             * Cài đặt sự kiện Double-Clicked: team member được chọn sẽ di chuyển sang
+             * bảng Available Employees.
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                // Đếm số lần click
+                if (e.getClickCount() > 1) {
+                    // Xác định dòng đang được chọn từ tọa độ của một điểm (Point) trên bảng
+                    int rowIndex = tableTeamMembers.rowAtPoint(e.getPoint());
+                    if (rowIndex != -1) {
+                        int[] rowIndexes = {rowIndex};
+                        onDeselect(rowIndexes);
+                    }
+                }
+            }
+        });
+
+        loadAvailableEmployees();
+    }
+
+    private void loadAvailableEmployees() {
+        modelAvailableEmployees.addEmployee(new Employee(0, "Messi", 500000));
+        modelAvailableEmployees.addEmployee(new Employee(2, "Ronaldo", 600000));
+        modelAvailableEmployees.addEmployee(new Employee(3, "Bale", 400000));
+        modelAvailableEmployees.addEmployee(new Employee(4, "Rooney", 300000));
+    }
+
+    private void onSelect(int[] rowIndexes) {
+        List<Employee> removedEmployees = modelAvailableEmployees.removeEmployee(rowIndexes);
+        modelTeamMembers.addEmployees(removedEmployees);
+    }
+
+    private void onDeselect(int[] rowIndexes) {
+        List<Employee> removedEmployees = modelTeamMembers.removeEmployee(rowIndexes);
+        modelAvailableEmployees.addEmployees(removedEmployees);
     }
 
     public static void main(String[] args) {
